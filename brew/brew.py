14540644013dfa1d9533d2f0f596ef08a4090f13
A-Generation <110045191+a-generation@users.noreply.github.com>
2024-12-18 00:15:52 +0500

    Update and rename brew.py to brew/brew.py

--- brew/brew.py
+++ brew/brew.py
@@ -1,104 +1,11 @@
+from brew.ingredients import INGREDIENT_SCORES
+from brew.temperature import IDEAL_TEMPERATURES
+from brew.brew_utils import update_score
+from brew.brew_utils import is_ideal_temperature
 import time
 import random
 
 class Brew:
-    INGREDIENT_SCORES = {
-    "tea": {
-        "jasmine": 3,
-        "mint": 4,
-        "lemon": 5,
-        "sugar": 2,
-        "honey": 4,
-        "water": 1,
-        "cardamom": 7,
-        "ginger": 6,
-        "cinnamon": 6,
-        "rose_petals": 5,
-        "black_tea": 3,
-        "green_tea": 4,
-        "oolong": 5,
-        "chamomile": 4,
-        "orange_peel": 5,
-        "lavender": 6,
-        "lime": 5,
-        "strawberry": 6,
-        "blueberry": 6,
-        "pineapple": 7,
-        "peach": 6
-    },
-    "coffee": {
-        "espresso": 5,
-        "milk": 3,
-        "caramel": 4,
-        "chocolate": 4,
-        "sugar": 2,
-        "water": 1,
-        "vanilla": 7,
-        "cinnamon": 6,
-        "nutmeg": 5,
-        "whipped_cream": 4,
-        "hazelnut": 5,
-        "almond_milk": 3,
-        "coconut_milk": 4,
-        "soy_milk": 3,
-        "maple_syrup": 6,
-        "pumpkin_spice": 6,
-        "dark_chocolate": 5,
-        "white_chocolate": 4
-    },
-    "latte": {
-        "espresso": 5,
-        "milk": 3,
-        "sugar": 2,
-        "vanilla": 4,
-        "cinnamon": 5,
-        "pumpkin_spice": 6,
-        "honey": 4,
-        "lavender": 5,
-        "almond_milk": 3,
-        "oat_milk": 3,
-        "maple_syrup": 6
-    },
-    "mocha": {
-        "espresso": 5,
-        "milk": 3,
-        "chocolate": 4,
-        "sugar": 2,
-        "dark_chocolate": 6,
-        "white_chocolate": 5,
-        "whipped_cream": 4,
-        "caramel": 5,
-        "hazelnut": 5
-    },
-    "fruit_tea": {
-        "water": 1,
-        "apple": 5,
-        "honey": 4,
-        "lemon": 5,
-        "orange": 5,
-        "strawberry": 6,
-        "blueberry": 6,
-        "raspberry": 7,
-        "peach": 6,
-        "pineapple": 7,
-        "mango": 7,
-        "passion_fruit": 8,
-        "cherry": 6,
-        "grape": 5,
-        "cinnamon": 5,
-        "ginger": 6,
-        "mint": 4,
-        "rose_petals": 5
-    },
-    }
-    IDEAL_TEMPERATURES = {
-        "tea": (70, 90),
-        "coffee": (60, 80),
-        "latte": (60, 70),
-        "mocha": (60, 70),
-        "fruit_tea": (70, 85),
-    }
-
     def __init__(self, brew_type, name):
         self.type = brew_type
         self.name = name
@@ -108,11 +15,13 @@ class Brew:
         self.waiting = False
         self.cooldown_count = 0
         self.flavor_profile = "regular"
+        self.weather_bonus_applied = False
+        self.seasonal_bonus_applied = False
 
     def add(self, ingredient):
-        if ingredient in self.INGREDIENT_SCORES.get(self.type, {}):
+        if ingredient in INGREDIENT_SCORES.get(self.type, {}):
             self.ingredients.append(ingredient)
-            self.update_score(ingredient)
+            update_score(self, ingredient)
             print(f"Added {ingredient} to {self.name}")
         else:
             print(f"Ingredient {ingredient} is not valid for {self.type}.")
@@ -120,7 +29,7 @@ class Brew:
     def remove(self, ingredient):
         if ingredient in self.ingredients:
             self.ingredients.remove(ingredient)
-            self.update_score(ingredient, remove=True)
+            update_score(self, ingredient, remove=True)
             print(f"Removed {ingredient} from {self.name}")
         else:
             print(f"{ingredient} is not in {self.name}")
@@ -130,7 +39,7 @@ class Brew:
             print("Temperature must be between 0 and 100 degrees Celsius.")
             return
         self.temperature = temperature
-        if self.is_ideal_temperature():
+        if is_ideal_temperature(self):
             self.score += 5
             print(f"Heated {self.name} to {temperature}°C (Ideal!)")
         else:
@@ -205,43 +114,57 @@ class Brew:
     def recipe(self):
         recipes = {
             "tea": "To make a perfect tea, boil water, add tea leaves, steep for 3-5 minutes, and enjoy!",
-            "coffee": "For a great coffee, brew espresso, add hot water, and mix with milk or cream.",
+            "coffee": "For a great coffee, brew espresso, add hot water, and mix with milk or cream as desired.",
             "latte": "To prepare a latte, steam milk and pour it over a shot of espresso.",
-            "mocha": "For a mocha, mix espresso with steamed milk and chocolate syrup, then top with whipped cream.",
-            "fruit_tea": "To make fruit tea, steep tea with fresh fruits and honey for sweetness.",
+            "mocha": "For a mocha, mix espresso with steamed milk and chocolate syrup.",
+            "fruit_tea": "To create fruit tea, steep tea with fresh fruits and sweeten to taste.",
         }
         if self.type in recipes:
             print("Recipe:", recipes[self.type])
         else:
-            print("No recipe available for this type of brew.")
+            print("No recipe available for this drink type.")
 
-    def update_score(self, ingredient, remove=False):
-        score_change = self.INGREDIENT_SCORES.get(self.type, {}).get(ingredient, 0)
-        if remove:
-            self.score -= score_change
+    def weather_bonus(self, condition):
+        bonus_ranges = {
+            "sunny": (3, 7),
+            "rainy": (5, 15),
+            "snowy": (4, 12),
+            "cloudy": (1, 4)
+        }
+
+        if self.weather_bonus_applied:
+            print(f"Weather bonus has already been applied to {self.name}.")
+            return
+
+        if condition in bonus_ranges:
+            min_bonus, max_bonus = bonus_ranges[condition]
+            bonus_value = random.randint(min_bonus, max_bonus) 
+            self.score += bonus_value
+            self.weather_bonus_applied = True 
+            print(f"Applied weather bonus of {bonus_value} to {self.name} due to {condition} weather.")
         else:
-            self.score += score_change
-        if len(self.ingredients) > 5:
-            print(f"Too many ingredients in {self.name}! The taste might be off.")
-            self.score -= 2
+            print(f"No bonus defined for weather condition: {condition}.")
 
-    def is_ideal_temperature(self):
-        if not self.temperature:
-            return False
-        ideal_range = self.IDEAL_TEMPERATURES.get(self.type, (0, 100))
-        return ideal_range[0] <= self.temperature <= ideal_range[1]
+    def seasonal_bonus(self, season):
+        bonus_ranges = {
+            "spring": (3, 8),
+            "summer": (5, 12),
+            "autumn": (2, 6),
+            "winter": (1, 5)
+        }
 
-    def seasonal_bonus(self):
-        current_month = time.localtime().tm_mon
-        if self.type == "mocha" and current_month in [11, 12, 1]:
-            self.score += 5
-            print("Seasonal bonus applied for winter mocha!")
+        if self.seasonal_bonus_applied:
+            print(f"Seasonal bonus has already been applied to {self.name}.")
+            return
 
-    def weather_bonus(self, weather_condition):
-        if weather_condition == "rainy":
-            if "mint" in self.ingredients:
-                self.score += 3
-                print("Rainy day bonus for mint!")
+        if season in bonus_ranges:
+            min_bonus, max_bonus = bonus_ranges[season]
+            bonus_value = random.randint(min_bonus, max_bonus)
+            self.score += bonus_value
+            self.seasonal_bonus_applied = True
+            print(f"Applied seasonal bonus of {bonus_value} to {self.name} due to {season} season.")
+        else:
+            print(f"No bonus defined for season: {season}.")
 
     def taste_review(self):
         if self.score > 20:
